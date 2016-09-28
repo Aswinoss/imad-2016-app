e12111f3e3c3f42a1ef986c8913f083f746d9fc1
Aswinoss <aswinosrise@gmail.com>
2016-09-28 13:41:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function (req,res){
-    res.send("Article 1 has been requested and it will be served here");//adding new urls 
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));//adding new urls 
 });
 
 

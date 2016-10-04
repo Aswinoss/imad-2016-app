2bb079c96159c8be98e3bdd3b09e9538e9dec88f
Aswinoss <aswinosrise@gmail.com>
2016-10-04 13:27:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,23 +5,106 @@ var path = require('path');
 var app = express();               //creating an object
 app.use(morgan('combined'));
 
+//js for reduced code
+
+var articleOne={
+    
+    title : 'Article-One | imad',
+    heading:'Article One',
+    content:` <p>
+                hai this is article one and we are here to test whether this works.<br>
+                It is great to have you people here and we are going to learn HTML5 and CSS3.<br>
+                Learn well and deploy your first webapp and aalways keep learning.<br>
+            </p>`,
+    date:'05-Sep-2016',
+};
+
+var articleTwo={
+    
+    title: 'Article-Two | imad',
+    heading:'Article Two',
+    content:` <p>
+               Hai this is article two and iam making a lot of progress.
+            </p>`,
+    date:'10-Sep-2016',
+};
+
+var articleThree={
+    
+    title : 'Article-Three | imad',
+    heading:'Article Three',
+    content:` <p>
+                great going guys you have completed major portion of this course and keep learning.
+            </p>`,
+    date:'15-Sep-2016',
+};
+
+
+function createTemplate(data){
+var title=data.title;
+var heading=data.heading;
+var date=data.date;
+var content=data.content;
+
+//creating common template for all three pages
+
+var htmlTemplate=`<!doctype html>
+<html>
+<head>
+      <link href="/ui/style.css" rel="stylesheet" />
+    
+  
+    <title>
+        ${title}
+    </title>
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1"  />
+    
+</head>
+<body>
+  <div class=container>   <!-- giving a class name for entire division -->
+     <div>
+        <a href="/">HOME</a>
+     </div>
+     <hr />
+     <div>
+         <h3>${heading}</h3>
+        
+     </div>
+    
+     <div>
+        <h6>${date}</h6>
+     </div>
+    
+     <div>
+        ${content}
+     </div>
+</div>
+</body>
+</html>
+`;
+return htmlTemplate;
+
+}
+
+
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.sendFile(path.join(--dirname,'ui','/ui/index.html'));
 });
 
 app.get('/article-one',function (req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(articleOne));
 });
 
 
 app.get('/article-two',function (req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+    res.send(createTemplate(articleTwo));
 });
 
 
 
 app.get('/article-three',function (req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+    res.send(createTemplate(aticleThree));
 });
 
 

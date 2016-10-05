d1875402f89dc71cc9ed7eca0b4b5949c942c083
Aswinoss <aswinosrise@gmail.com>
2016-10-06 00:35:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,11 +19,11 @@ function moveLeft()
     
     marginRight=marginRight + 1;
     marginRight=marginRight + 'px';
-    img.style.marginRight=marginRight;
+    img.style.marginRight = marginRight;
 }
 
 img.onclick=function()
 {
-    var interval=setInterval(moveLeft,50);
+    var interval=setInterval(moveLeft , 50);
 };
 

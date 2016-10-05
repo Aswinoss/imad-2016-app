96cf1bb536a7e930275962fc797396326134c6e0
Aswinoss <aswinosrise@gmail.com>
2016-10-05 23:47:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,11 @@
 console.log('the page is primed and ready to go!!!');
 var rep=window.prompt("welcome Aswinoss");
+if(rep=="aswinokuruion1")
+{
 window.alert("Thanx");
 var element=document.getElementById("mainText");
-element.innerHTML=`We are Legion
-                   We dont Forget
-                   We dont Forgive
+element.innerHTML=`We are Legion<br />
+                   We dont Forget<br />
+                   We dont Forgive<br />
                    Expect us!`;
+}

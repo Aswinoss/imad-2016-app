5aba3097f4197752c71543fefb5220e812288bb5
Aswinoss <aswinosrise@gmail.com>
2016-10-08 06:31:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,18 @@
 console.log('the page is primed and ready to go!!!');
 
 var button=document.getElementById("counter");
+var counter=0;
 button.onclick=function(){
+     counter=counter+1;
+    
+    
+    var span=document.getElementById("count");
+    span.innerHTML=counter.toString();
+    
+    
+
+
+/*
     //create new request object
     
     var request=new XMLHttpRequest();
@@ -22,10 +33,9 @@ button.onclick=function(){
     request.open('GET','https://aswinoss.imad.hasura-app.io/counter',true);
     request.send(null);
     
-    
+ */   
     
 };
-
 
 
 

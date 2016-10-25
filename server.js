e7a2c629781e78f7ad94eed5656041ada8872a5e
Aswinoss <aswinosrise@gmail.com>
2016-10-25 22:48:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -158,12 +158,12 @@ app.get('/article/:articleName',function (req,res){
             res.status(500).send(err.toString());
         }
         else{
-            if(result.rows.length===0){
+            if(result.rows.length === 0){
                 res.status(404).send('Article not found');
             }
             else{
                  var articleData = result.rows[0];
-                 res.send(createTemplate(articelData);
+                 res.send(createTemplate(articelData));
                 
             }
         }
